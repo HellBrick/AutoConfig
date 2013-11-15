07976d415ecbb12709ce6e2f928ab73ba2b83b1e
HellBrick <hellbrick@gmail.com>
2013-11-15 15:15:52 +0400

    Documented AutoConfigManager class

--- AutoConfig/AutoConfigManager.cs
+++ AutoConfig/AutoConfigManager.cs
@@ -9,13 +9,31 @@ using AutoConfig.Internal;
 
 namespace AutoConfig
 {
+	/// <summary>
+	/// Provides routines to load configuration sections as strongly typed POCO objects.
+	/// </summary>
 	public static class AutoConfigManager
 	{
+		/// <summary>
+		/// Gets the section with the name that matches the name of the class passed as a type parameter.
+		/// </summary>
+		/// <typeparam name="T">Type of the object to load the section data to. Must be a non-abstract class with parameterless constructor.</typeparam>
+		/// <exception cref="AutoConfig.SectionNotFoundException">Failed to find a section with the specified name.</exception>
+		/// <exception cref="AutoConfig.TypeConstructionException">Failed ot create an instance of one of the types of the T's properties.</exception>
+		/// <exception cref="AutoConfig.PropertyParsingException">Failed to parse one of the class properties from the string found in the configuration file.</exception>
 		public static T GetSection<T>() where T: new()
 		{
 			return GetSection<T>( typeof( T ).Name );
 		}
 
+		/// <summary>
+		/// Gets the section with the specified name from the configuration.
+		/// </summary>
+		/// <typeparam name="T">Type of the object to load the section data to. Must be a non-abstract class with parameterless constructor.</typeparam>
+		/// <param name="sectionName">Name of the section to find in the configuration file.</param>
+		/// <exception cref="AutoConfig.SectionNotFoundException">Failed to find a section with the specified name.</exception>
+		/// <exception cref="AutoConfig.TypeConstructionException">Failed ot create an instance of one of the types of the T's properties.</exception>
+		/// <exception cref="AutoConfig.PropertyParsingException">Failed to parse one of the class properties from the string found in the configuration file.</exception>
 		public static T GetSection<T>( string sectionName ) where T: new()
 		{
 			T section = new T();
